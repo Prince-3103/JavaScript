1874e718102c3ef55d0e53f637a551fdc1170483
Prince-3103 <p2577430@gmail.com>
2026-06-26 00:04:09 +0530

    Showing notification for short time

--- closures/script.js
+++ closures/script.js
@@ -3,19 +3,31 @@ function createToaster(config){
         let msg = document.createElement("div");
         msg.textContent = message
         msg.classList.add("toast");
+ 
+        if(config.theme === "dark"){
+            msg.style.backgroundColor = "gray";
+            msg.style.color = "white";
+        } 
+        else{
+            msg.style.backgroundColor = "white";
+            msg.style.color = "black";
+        }
 
-        let parent = document.createElement("div");
-        parent.classList.add("parent");
-        parent.appendChild(msg);
-        document.body.appendChild(parent);
+        // msg.style.positionX
+
+        document.querySelector(".parent").appendChild(msg);
+
+        setTimeout(() =>{
+            document.querySelector(".parent").removeChild(msg)
+        }, config.duration * 1000);
     }
 }
 
 let noti = createToaster({
     positionX: "right",
     positionY: "top",
-    theme: "dark",
+    theme: "light",
     duration: 3
 });
 
-noti("If u never taste bad apple you'll never appreciate a good one.")
+noti("Prince sent you request")
